2f89ecdbd5696d373268ec8e73e7e673dbd89419
PManyatha <manyathap@Testvagrant.com>
2024-09-30 16:43:09 +0530

    DataTypeDetective

---
+++ src/DataTypeDetective.java
@@ -0,0 +1,13 @@
+public class DataTypeDetective {
+    public static void main(String[] args){
+        int a =1;
+        double b=10.67;
+        char c='M';
+        boolean d=true;
+
+        System.out.println(a+" is an Integer");
+        System.out.println(b+" is Double");
+        System.out.println(c+" is Character");
+        System.out.println(d+" is Boolean");
+    }
+}
