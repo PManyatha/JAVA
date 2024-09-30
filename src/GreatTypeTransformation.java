8251815773152b36dab29bf15ca0081e85e8e7ef
PManyatha <manyathap@Testvagrant.com>
2024-09-30 16:46:54 +0530

    GreatTypeTransformation

---
+++ src/GreatTypeTransformation.java
@@ -0,0 +1,9 @@
+public class GreatTypeTransformation {
+    public static void main(String[] args) {
+    double myDouble = 9.99;
+    int convertedInt = (int) myDouble;
+
+        System.out.println("Converted double to int: " + convertedInt);
+
+}
+}
