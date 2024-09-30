b36570b6ab796bda74eb733762836f36cc2fd53e
PManyatha <manyathap@Testvagrant.com>
2024-09-30 16:19:03 +0530

    RightTypeRiddle

---
+++ src/RightTypeRiddle.java
@@ -0,0 +1,13 @@
+public class RightTypeRiddle {
+    public static void main(String[] args){
+        int a =1;
+        Double b=10.67;
+        char c='M';
+        Boolean d=true;
+
+        System.out.println(a+" is an Integer");
+        System.out.println(b+" is Double");
+        System.out.println(c+" is Character");
+        System.out.println(d+" is Boolean");
+    }
+}
