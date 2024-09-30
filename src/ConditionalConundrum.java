3c19dd00c2ba2a2f4d78db4576fb94b1801bfb01
PManyatha <manyathap@Testvagrant.com>
2024-09-30 16:56:16 +0530

    ConditionalConundrum

---
+++ src/ConditionalConundrum.java
@@ -0,0 +1,14 @@
+public class ConditionalConundrum {
+    public static void main(String[] args){
+        int a=5;
+        if(a < 0){
+            System.out.println("The number is negative");
+        } else if (a > 0) {
+            System.out.println("The number is positive");
+
+        }
+        else{
+            System.out.println("The number is zero");
+        }
+    }
+}
