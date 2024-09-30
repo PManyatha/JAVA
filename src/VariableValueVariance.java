625e8f41d598c9eedcc828a76a4fa9da4be17846
PManyatha <manyathap@Testvagrant.com>
2024-09-30 16:38:38 +0530

    VariableValueVariance

--- src/VariableValueVariance.java
+++ src/VariableValueVariance.java
@@ -1,9 +1,9 @@
 public class VariableValueVariance {
     public static void main(String[]args){
 
-        String s="5";
+        int s=5;
         System.out.print(s);
-        s="7";
+        s=7;
         System.out.print(s);
 
     }
